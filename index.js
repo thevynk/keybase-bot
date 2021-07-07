5ceb5c2369beb3f44830c705941783624621cc22
Siddharth Arumugam <0xmrnight@gmail.com>
2021-07-07 14:04:03 +0530

    Prettfiy index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const Bot = require('keybase-bot')
-
+const Bot = require("keybase-bot");
+require("dotenv").config();
 
 async function main() {
-    const bot = new Bot()
-    try {
-        const username = process.env.USERNAME
-        const paperkey = process.env.PAPER_KEY
-        await bot.init(username, paperkey, {verbose: false})
-        console.log(`bot is online as ${bot.myInfo().username}`)
-        const channel = { name: 'general'}
-        const message = { body: `Hello, I'm ${bot.myInfo().username}`}
-        await bot.chat.send(channel, message)
-        console.log(`successfully sent the message as ${bot.myInfo().username} from the device ${bot.myInfo().devicename}`)
-    }
-    catch (error) {
-        console.log(error)
-    }
-    finally {
-        await bot.deinit()
-    }
+  const bot = new Bot();
+  try {
+    const username = process.env.USERNAME;
+    const paperkey = process.env.PAPER_KEY;
+
+    await bot.init(username, paperkey, { verbose: false });
+    console.log(`I have logged in as ${bot.myInfo().username}`);
+    const channel = { name: "general" };
+    const message = { body: `Hello, I'm ${bot.myInfo().username}` };
+    await bot.chat.send(channel, message);
+    console.log(
+      `Successfully sent the message as ${
+        bot.myInfo().username
+      } from the device ${bot.myInfo().devicename}`
+    );
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await bot.deinit();
+  }
 }
 
-main()
+main();
